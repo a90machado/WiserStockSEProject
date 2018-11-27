bf064f6b9c643a054fb9dd83955dc257fa9eeeb5
a90machado <a90machado@gmail.com>
2018-11-27 17:33:18 +0000

    Initial commit

---
+++ src/io/altar/jseproject/model/Entity.java
@@ -0,0 +1,18 @@
+package io.altar.jseproject.model;
+//Class that contain common attributes between Product and Shelf.
+
+public class Entity {
+	
+	// Attributes:
+	private long id;
+
+	// Get and Set:
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
